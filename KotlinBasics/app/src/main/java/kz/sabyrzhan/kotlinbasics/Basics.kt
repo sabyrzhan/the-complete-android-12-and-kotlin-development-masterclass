8e6099a7bb22bcd99aaeeeeb5128223f0b17576d
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-10-20 22:20:54 +0600

    Kotlin basics - add for loop demos

---
+++ KotlinBasics/app/src/main/java/kz/sabyrzhan/kotlinbasics/Basics.kt
@@ -0,0 +1,45 @@
+package kz.sabyrzhan.kotlinbasics
+
+fun main() {
+    forEachRangeWithMethodWay()
+}
+
+fun forEachRangeWithMethodWay() {
+    1.rangeTo(10).step(2).forEach {
+        println("For each method: $it")
+    }
+}
+
+fun forEachWithInAndStep() {
+    for (num in 1..10 step 2) {
+        println("WIth step: $num")
+    }
+}
+
+fun forEachWithDownTo() {
+    for (num in 10 downTo 1) {
+        println("Downto: $num")
+    }
+}
+
+fun forEachWithUntilDemo() {
+    for (num in 1 until 10) {
+        println(num)
+    }
+}
+
+fun forEachWithInDemo() {
+    for (num in 1..3) {
+        println(num)
+    }
+}
+
+fun whenDemo() {
+    val age = 220
+    when (age) {
+        in 1..10 -> println("Age is 1 to 10")
+        in 20..200 -> println("Age is 20 to 200")
+        220, 230 -> println("Something else")
+        else -> println("Something diff")
+    }
+}
