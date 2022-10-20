40492bcb024cd015505d3ba7a40427cfe0bb622c
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-10-20 17:46:28 +0600

    PeopleCounter: Add counter

--- PeopleCounter/app/src/main/java/kz/sabyrzhan/peoplecounter/MainActivity.kt
+++ PeopleCounter/app/src/main/java/kz/sabyrzhan/peoplecounter/MainActivity.kt
@@ -2,10 +2,22 @@ package kz.sabyrzhan.peoplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.myButton)
+        val textView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        myButton.setOnClickListener {
+            timesClicked++
+            textView.text = timesClicked.toString()
+            Toast.makeText(this, "this is the toast!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
