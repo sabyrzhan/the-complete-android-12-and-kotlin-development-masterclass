0db1788082c1ad8dd17eacc8137e6f929ef56e0a
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-10-20 22:43:29 +0600

    Kotlin basics - add function/method signature

--- KotlinBasics/app/src/main/java/kz/sabyrzhan/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/kz/sabyrzhan/kotlinbasics/Basics.kt
@@ -1,7 +1,11 @@
 package kz.sabyrzhan.kotlinbasics
 
 fun main() {
-    forEachRangeWithMethodWay()
+    println(add(1, 2))
+}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
 }
 
 fun forEachRangeWithMethodWay() {
